e281264854671074aa3638a59a9bec1a512a372f
patrickjhlynch <patrickjhlynch@gmail.com>
2018-02-19 15:12:42 +1300

    finished backup/restore form

--- TractionAir/TractionAir/Forms/backupRestoreForm.Designer.cs
+++ TractionAir/TractionAir/Forms/backupRestoreForm.Designer.cs
@@ -29,13 +29,127 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(backupRestoreForm));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backupLocationTextbox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.backupButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.restoreButton = new System.Windows.Forms.Button();
+            this.browseButton2 = new System.Windows.Forms.Button();
+            this.restoreLocationTextbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.backupButton);
+            this.groupBox1.Controls.Add(this.browseButton);
+            this.groupBox1.Controls.Add(this.backupLocationTextbox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(553, 102);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Backup Database";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Location";
+            // 
+            // backupLocationTextbox
+            // 
+            this.backupLocationTextbox.Location = new System.Drawing.Point(93, 31);
+            this.backupLocationTextbox.Name = "backupLocationTextbox";
+            this.backupLocationTextbox.Size = new System.Drawing.Size(324, 20);
+            this.backupLocationTextbox.TabIndex = 1;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(438, 28);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 2;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // backupButton
+            // 
+            this.backupButton.Enabled = false;
+            this.backupButton.Location = new System.Drawing.Point(438, 57);
+            this.backupButton.Name = "backupButton";
+            this.backupButton.Size = new System.Drawing.Size(75, 23);
+            this.backupButton.TabIndex = 3;
+            this.backupButton.Text = "Backup";
+            this.backupButton.UseVisualStyleBackColor = true;
+            this.backupButton.Click += new System.EventHandler(this.backupButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.restoreButton);
+            this.groupBox2.Controls.Add(this.browseButton2);
+            this.groupBox2.Controls.Add(this.restoreLocationTextbox);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(13, 121);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(553, 102);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Restore Database";
+            // 
+            // restoreButton
+            // 
+            this.restoreButton.Enabled = false;
+            this.restoreButton.Location = new System.Drawing.Point(438, 57);
+            this.restoreButton.Name = "restoreButton";
+            this.restoreButton.Size = new System.Drawing.Size(75, 23);
+            this.restoreButton.TabIndex = 3;
+            this.restoreButton.Text = "Restore";
+            this.restoreButton.UseVisualStyleBackColor = true;
+            this.restoreButton.Click += new System.EventHandler(this.restoreButton_Click);
+            // 
+            // browseButton2
+            // 
+            this.browseButton2.Location = new System.Drawing.Point(438, 28);
+            this.browseButton2.Name = "browseButton2";
+            this.browseButton2.Size = new System.Drawing.Size(75, 23);
+            this.browseButton2.TabIndex = 2;
+            this.browseButton2.Text = "Browse";
+            this.browseButton2.UseVisualStyleBackColor = true;
+            this.browseButton2.Click += new System.EventHandler(this.browseButton2_Click);
+            // 
+            // restoreLocationTextbox
+            // 
+            this.restoreLocationTextbox.Location = new System.Drawing.Point(93, 31);
+            this.restoreLocationTextbox.Name = "restoreLocationTextbox";
+            this.restoreLocationTextbox.Size = new System.Drawing.Size(324, 20);
+            this.restoreLocationTextbox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Location";
             // 
             // backupRestoreForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(578, 414);
+            this.ClientSize = new System.Drawing.Size(578, 264);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -43,10 +157,25 @@
             this.Name = "backupRestoreForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Backup/Restore Database";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button backupButton;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox backupLocationTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button restoreButton;
+        private System.Windows.Forms.Button browseButton2;
+        private System.Windows.Forms.TextBox restoreLocationTextbox;
+        private System.Windows.Forms.Label label2;
     }
 }
--- TractionAir/TractionAir/Forms/backupRestoreForm.cs
+++ TractionAir/TractionAir/Forms/backupRestoreForm.cs
@@ -7,14 +7,104 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace TractionAir.Forms
 {
     public partial class backupRestoreForm : Form
     {
+        SqlConnection con = new SqlConnection(ECU_Manager.connection("ecuSettingsDB_CS"));
+
         public backupRestoreForm()
         {
             InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                backupLocationTextbox.Text = dlg.SelectedPath;
+                backupButton.Enabled = true;
+            }
+        }
+
+        private void backupButton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (backupLocationTextbox.Text == string.Empty)
+                {
+                    MessageBox.Show("Please enter backup file location", "Invalid Input");
+                }
+                else
+                {
+                    if (con.State != ConnectionState.Open)
+                    {
+                        con.Open();
+                    }
+                    string database = con.Database.ToString();
+                    string cmd = "BACKUP DATABASE [" + database + "] TO DISK='" + backupLocationTextbox.Text + "\\" + "Database" + "-" + DateTime.Now.ToString("yyyy-MM-dd--HH-mm-ss") + ".bak'";
+
+                    using(SqlCommand command = new SqlCommand(cmd, con))
+                    {
+
+                        command.ExecuteNonQuery();
+                        con.Close();
+                        MessageBox.Show("Database backup completed successfully");
+                        backupButton.Enabled = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error");
+            }
+        }
+
+        private void browseButton2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "SQL SERVER database backup files|*.bak";
+            dlg.Title = "Database Restore";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                restoreLocationTextbox.Text = dlg.FileName;
+                restoreButton.Enabled = true;
+            }
+        }
+
+        private void restoreButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (con.State != ConnectionState.Open)
+                {
+                    con.Open();
+                }
+                string database = con.Database.ToString();
+
+                string sqlStmt2 = string.Format("ALTER DATABASE [" + database + "] SET SINGLE_USER WITH ROLLBACK IMMEDIATE");
+                SqlCommand bu2 = new SqlCommand(sqlStmt2, con);
+                bu2.ExecuteNonQuery();
+
+                string sqlStmt3 = "USE MASTER RESTORE DATABASE [" + database + "] FROM DISK='" + restoreLocationTextbox.Text + "'WITH REPLACE;";
+                SqlCommand bu3 = new SqlCommand(sqlStmt3, con);
+                bu3.ExecuteNonQuery();
+
+                string sqlStmt4 = string.Format("ALTER DATABASE [" + database + "] SET MULTI_USER");
+                SqlCommand bu4 = new SqlCommand(sqlStmt4, con);
+                bu4.ExecuteNonQuery();
+
+                MessageBox.Show("Database restoration completed successfully");
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error");
+            }
         }
     }
 }
